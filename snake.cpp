c79fde608f7e30dd570bc550c58d82b725c15610
Ravi Dev Pandey <pandeyravidev@gmail.com>
2022-01-03 13:53:17 +0530

    add sprites and fix formatting

--- snake.cpp
+++ snake.cpp
@@ -2,13 +2,47 @@
 
 Snake::Snake()
 {
-    if (!moveSoundBuffer.loadFromFile("assets/music/move.wav"))
+    sf::Texture *headRightTexture = new sf::Texture;
+    sf::Texture *headLeftTexture = new sf::Texture;
+    sf::Texture *headUpTexture = new sf::Texture;
+    sf::Texture *headDownTexture = new sf::Texture;
+    sf::Texture *tailRightTexture = new sf::Texture;
+    sf::Texture *tailLeftTexture = new sf::Texture;
+    sf::Texture *tailUpTexture = new sf::Texture;
+    sf::Texture *tailDownTexture = new sf::Texture;
+    sf::Texture *bodyHorizontalTexture = new sf::Texture;
+    sf::Texture *bodyVerticalTexture = new sf::Texture;
+    sf::SoundBuffer *moveSoundBuffer = new sf::SoundBuffer;
+
+    if (!moveSoundBuffer->loadFromFile("assets/music/move.wav") ||
+        !headRightTexture->loadFromFile("assets/images/head_right.png") ||
+        !headLeftTexture->loadFromFile("assets/images/head_left.png") ||
+        !headUpTexture->loadFromFile("assets/images/head_up.png") ||
+        !headDownTexture->loadFromFile("assets/images/head_down.png") ||
+        !tailRightTexture->loadFromFile("assets/images/tail_left.png") ||
+        !tailLeftTexture->loadFromFile("assets/images/tail_right.png") ||
+        !tailUpTexture->loadFromFile("assets/images/tail_down.png") ||
+        !bodyHorizontalTexture->loadFromFile("assets/images/body_horizontal.png") ||
+        !bodyVerticalTexture->loadFromFile("assets/images/body_vertical.png") ||
+        !tailDownTexture->loadFromFile("assets/images/tail_up.png"))
     {
         std::cout << sf::err;
     }
     else
     {
-        moveSound.setBuffer(moveSoundBuffer);
+        moveSound.setBuffer(*moveSoundBuffer);
+
+        headRight.setTexture(*headRightTexture);
+        headLeft.setTexture(*headLeftTexture);
+        headUp.setTexture(*headUpTexture);
+        headDown.setTexture(*headDownTexture);
+        tailRight.setTexture(*tailRightTexture);
+        tailLeft.setTexture(*tailLeftTexture);
+        tailUp.setTexture(*tailUpTexture);
+        tailDown.setTexture(*tailDownTexture);
+
+        bodyHorizontal.setTexture(*bodyHorizontalTexture);
+        bodyVertical.setTexture(*bodyVerticalTexture);
     }
 };
 
@@ -57,9 +91,68 @@ bool Snake::checkEat(sf::Vector2f foodPos)
     return false;
 }
 
-sf::Vector2f &Snake::getHead()
+sf::Vector2f &Snake::getHeadPos()
 {
     return bodyArray[0];
+}
+
+sf::Sprite &Snake::getHeadSprite()
+{
+    if (movements[0].x == 5)
+    {
+        return headRight;
+    }
+    else if (movements[0].x == -5)
+    {
+        return headLeft;
+    }
+    else if (movements[0].y == -5)
+    {
+        return headUp;
+    }
+    else
+    {
+        return headDown;
+    }
+}
+
+sf::Sprite &Snake::getTailSprite()
+{
+    if (movements[movements.size() - 1].x == 5)
+    {
+        return tailRight;
+    }
+    else if (movements[movements.size() - 1].x == -5)
+    {
+        return tailLeft;
+    }
+    else if (movements[movements.size() - 1].y == -5)
+    {
+        return tailUp;
+    }
+    else
+    {
+        return tailDown;
+    }
+}
+
+sf::Sprite &Snake::getBodySprite(int index)
+{
+    if (index == 0)
+        return getHeadSprite();
+    else if (index == bodyArray.size() - 1)
+        return getTailSprite();
+    else
+    {
+        if (movements[index].x)
+        {
+            return bodyHorizontal;
+        }
+        else
+        {
+            return bodyVertical;
+        }
+    }
 }
 
 std::vector<sf::Vector2f> &Snake::getSnakeBody()
--- snake.h
+++ snake.h
@@ -11,16 +11,27 @@
 class Snake
 {
 private:
-    std::vector<sf::Vector2f> bodyArray{sf::Vector2f{330, 240}};
+    std::vector<sf::Vector2f> bodyArray{sf::Vector2f{330, 240},
+                                        sf::Vector2f{330 - blockWidth, 240}};
     std::vector<sf::Vector2f> bodyArrayTemp{sf::Vector2f{330, 240}};
     sf::Vector2f movement{0, 0};
-    std::vector<sf::Vector2f> movements{{0, 0}};
+    std::vector<sf::Vector2f> movements{{5, 0}, {5, 0}};
 
     sf::Sound moveSound;
-    sf::SoundBuffer moveSoundBuffer;
 
     sf::Sprite head;
     sf::Sprite tail;
+
+    sf::Sprite headRight;
+    sf::Sprite headLeft;
+    sf::Sprite headUp;
+    sf::Sprite headDown;
+    sf::Sprite tailRight;
+    sf::Sprite tailLeft;
+    sf::Sprite tailUp;
+    sf::Sprite tailDown;
+    sf::Sprite bodyHorizontal;
+    sf::Sprite bodyVertical;
 
 public:
     Snake();
@@ -29,8 +40,13 @@ public:
     void move();
     void setMovement(sf::Vector2f newMovement);
     bool checkEat(sf::Vector2f foodPos);
-    sf::Vector2f &getHead();
+
     std::vector<sf::Vector2f> &getSnakeBody();
+
+    sf::Vector2f &getHeadPos();
+    sf::Sprite &getHeadSprite();
+    sf::Sprite &getTailSprite();
+    sf::Sprite &getBodySprite(int index);
 };
 
 #endif
